9c3f24ad0e72636264538c680db78dbfc3c9ac1c
Mahmoud Owaimer <95885650+MahmoudOwaimer@users.noreply.github.com>
2022-02-14 12:17:13 +0200

    add - character structure

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,17 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('KillTheBoss');
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Cow", "Monkey", "Dog", "Wolf", "Lion"],       // Names
+    [
+      "https://image.shutterstock.com/image-vector/cow-head-artwork-illustration-glow-600w-1517650835.jpg",
+      "https://image.shutterstock.com/image-vector/monkey-artwork-glow-dark-600w-1517951807.jpg",
+      "https://image.shutterstock.com/image-vector/glow-color-gradation-ethnic-dog-600w-1518846761.jpg",
+      "https://image.shutterstock.com/image-vector/wolf-head-color-glowing-artwork-600w-1517648312.jpg",
+      "https://image.shutterstock.com/image-vector/glow-color-lion-head-artwork-600w-1517955278.jpg"
+    ],
+    [100, 200, 300, 400, 500],                    // HP values
+    [10, 25, 50, 75, 90]                       // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
