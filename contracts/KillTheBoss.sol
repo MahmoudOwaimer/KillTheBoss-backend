c10296b3ea8d387f8e747ebeacd135415ef773e1
Mahmoud Owaimer <95885650+MahmoudOwaimer@users.noreply.github.com>
2022-02-14 12:25:31 +0200

    add - Mint the NFTs Feature

--- contracts/KillTheBoss.sol
+++ contracts/KillTheBoss.sol
@@ -2,9 +2,18 @@
 
 pragma solidity ^0.8.4;
 
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
-contract KillTheBoss {
+// Our contract inherits from ERC721, which is the standard NFT contract!
+contract KillTheBoss is ERC721 {
+
   // We'll hold our character's attributes in a struct. Feel free to add
   // whatever you'd like as an attribute! (ex. defense, crit chance, etc).
   struct CharacterAttributes {
@@ -15,10 +24,23 @@ contract KillTheBoss {
     uint maxHp;
     uint attackDamage;
   }
+
+  // The tokenId is the NFTs unique identifier, it's just a number that goes
+  // 0, 1, 2, 3, etc.
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
   // A lil array to help us hold the default data for our characters.
   // This will be helpful when we mint new characters and need to know
   // things like their HP, AD, etc.
   CharacterAttributes[] defaultCharacters;
+
+  // We create a mapping from the nft's tokenId => that NFTs attributes.
+  mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+  // A mapping from an address => the NFTs tokenId. Gives me an ez way
+  // to store the owner of the NFT and reference it later.
+  mapping(address => uint256) public nftHolders;
 
   // Data passed in to the contract when it's first created initializing the characters.
   // We're going to actually pass these values in from run.js.
@@ -26,9 +48,9 @@ contract KillTheBoss {
     string[] memory characterNames,
     string[] memory characterImageURIs,
     uint[] memory characterHp,
-    uint[] memory characterAttackDmg
-  )
+    uint[] memory characterAttackDmg) ERC721("Heroes", "HERO")
   {
+
     // Loop through all the characters, and save their values in our contract so
     // we can use them later when we mint our NFTs.
     for(uint i = 0; i < characterNames.length; i += 1) {
@@ -42,7 +64,43 @@ contract KillTheBoss {
       }));
 
       CharacterAttributes memory c = defaultCharacters[i];
+
+      // Hardhat's use of console.log() allows up to 4 parameters in any order of following types: uint, string, bool, address
       console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
     }
+
+    // I increment _tokenIds here so that my first NFT has an ID of 1.
+    // More on this in the lesson!
+    _tokenIds.increment();
   }
+
+  // Users would be able to hit this function and get their NFT based on the
+  // characterId they send in!
+  function mintCharacterNFT(uint _characterIndex) external {
+    // Get current tokenId (starts at 1 since we incremented in the constructor).
+    uint256 newItemId = _tokenIds.current();
+
+    // The magical function! Assigns the tokenId to the caller's wallet address.
+    _safeMint(msg.sender, newItemId);
+
+    // We map the tokenId => their character attributes. More on this in
+    // the lesson below.
+    nftHolderAttributes[newItemId] = CharacterAttributes({
+      characterIndex: _characterIndex,
+      name: defaultCharacters[_characterIndex].name,
+      imageURI: defaultCharacters[_characterIndex].imageURI,
+      hp: defaultCharacters[_characterIndex].hp,
+      maxHp: defaultCharacters[_characterIndex].maxHp,
+      attackDamage: defaultCharacters[_characterIndex].attackDamage
+    });
+
+    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+    // Keep an easy way to see who owns what NFT.
+    nftHolders[msg.sender] = newItemId;
+
+    // Increment the tokenId for the next person that uses it.
+    _tokenIds.increment();
+  }
+
 }
